3645555fd6caeca564da287f579b0a9bda5cc789
nidhi7733 <pnidhi2@gmail.com>
2024-02-09 21:58:38 +0545

    input type program created

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, res;
+    cout<<"Enter the value of number one: ";
+    cin>>num1;
+    cout<<"Enter the value of number two: ";
+    cin>>num2;
+    res = num1+num2;
+    cout<<"The sum of two numbers is :"<<res;
+
+    return 0;
+}
